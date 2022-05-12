808ccf1144710d38578dc83b14a060bdcbc45d88
gabrielraeder <gabrielraeder@outlook.com.br>
2022-05-12 16:18:23 -0300

    switchcase

---
+++ fundamentos/bloco4/dia4.1/ex04/script2.js
@@ -0,0 +1,8 @@
+let weekDay = 'Sábado';
+
+if (weekDay === 'Segunda' || weekDay === 'Terça' || weekDay === 'Quarta' || weekDay === 'Quinta' || weekDay === 'Sexta') {
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
+}
+else {
+    console.log("FINALMENTE, descanso merecido UwU");
+}
