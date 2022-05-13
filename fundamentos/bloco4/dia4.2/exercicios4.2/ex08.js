f19c976e44f5444ddf67e3d06d5fee6b977f5c15
gabrielraeder <gabrielraeder@outlook.com.br>
2022-05-13 15:36:57 -0300

    exercicios dia 4.2

---
+++ fundamentos/bloco4/dia4.2/exercicios4.2/ex08.js
@@ -0,0 +1,6 @@
+let list = []
+
+for (let index = 1; index <= 25; index+=1) {
+    list.push(index)    
+}
+console.log(list);
---
+++ fundamentos/bloco4/dia4.2/exercicios4.2/ex09.js
@@ -0,0 +1,8 @@
+let list = []
+
+for (let index = 1; index <= 25; index+=1) {
+    list.push(index)    
+}
+for (let i = 0; i < 25; i += 1) {
+    console.log(list[i]/2);
+}
