9da594f5954a1df564830c459b478ddd92847616
gabrielraeder <gabrielraeder@outlook.com.br>
2022-05-17 21:29:21 -0300

    bonus

---
+++ fundamentos/bloco4/dia4.4/exercicios/bonus/bonus05.js
@@ -0,0 +1,39 @@
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+};
+
+
+for (item of moradores.blocoUm) {
+    console.log(item.nome, item.sobrenome);
+}
+for (item of moradores.blocoDois) {
+    console.log(item.nome, item.sobrenome);
+}
+
