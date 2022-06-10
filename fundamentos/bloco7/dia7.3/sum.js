203b795932fd58bc901a36d988f2e3cd8fe00789
gabrielraeder <gabrielraeder@outlook.com.br>
2022-06-10 16:18:56 -0300

    ex01 completo

---
+++ fundamentos/bloco7/dia7.3/sum.js
@@ -0,0 +1,19 @@
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+module.exports = sum, myRemove;
