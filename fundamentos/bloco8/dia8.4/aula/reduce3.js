d1927fed5e1d2ad78c79f81632fa1127ca27b9f4
gabrielraeder <gabrielraeder@outlook.com.br>
2022-06-23 15:46:50 -0300

    aula

---
+++ fundamentos/bloco8/dia8.4/aula/reduce3.js
@@ -0,0 +1,102 @@
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+// Minha solução
+
+const relatorio = () => estudantes.map((aluno) => {
+  const maior = (bigger, number) => ((bigger > number.nota) ? bigger : number.nota);
+  const obj = {};
+  obj.name = aluno.nome;
+  const maiorNota = aluno.materias.reduce(maior, 0)
+  const materia = aluno.materias.find((element) => maiorNota === element.nota)
+  obj.materia = materia.name
+  return obj
+})
+
+console.log(relatorio());
+
+
+// Solução course
+
+const getBestClass = (acc, materia) => {
+  if (acc.nota > materia.nota) return acc;
+  return materia;
+};
+
+const reportBetter = (students) => students.map((student) => ({
+  name: student.nome,
+  materia: student.materias.reduce(getBestClass).name}));
+
+console.log(reportBetter(estudantes));
