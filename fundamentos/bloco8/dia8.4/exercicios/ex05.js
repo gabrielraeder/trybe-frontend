1f9e1947e080c5d7d99b77545ac50abfafa8de4f
gabrielraeder <gabrielraeder@outlook.com.br>
2022-06-23 19:02:57 -0300

    dia completo

---
+++ fundamentos/bloco8/dia8.4/exercicios/ex05.js
@@ -0,0 +1,17 @@
+// 5- Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, curr) =>
+     acc + curr.split('').reduce((acumulator, current) => {
+        if (current === 'a' || current === 'A') return acumulator + 1;
+        return acumulator;
+     }, 0), 0);
+}
+
+console.log(containsA());
