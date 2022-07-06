dbcf2e81453ea3df85d64d38b97ddd11c8bceddf
gabrielraeder <gabrielraeder@outlook.com.br>
2022-07-06 18:40:53 -0300

    dia9.2 completo

--- front-end/bloco9/dia9.2/exercicios/ex02/api.js
+++ front-end/bloco9/dia9.2/exercicios/ex02/api.js
@@ -1,23 +1,26 @@
 const apiCoin = 'https://api.coincap.io/v2/assets'
 // const fetch = require('node-fetch');
+const rates = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/brl.json'
 
 const fetchCoins = async () => {
-  const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  };
 
-  const {data} = await fetch(apiCoin, myObject)
+  const {data} = await fetch(apiCoin)
     .then((item) => item.json())
     .then((coin) => coin)
     .catch((error) => console.log(`Erro encontrado ${error}`));
+  
+  
+  const real = await fetch(rates)
+    .then((moedas) => moedas.json())
+    .then((moeda) => moeda)
+    .catch((erro) => console.log(`Erro encontrado ${erro}`));
   
   const firstTen = data.filter((element, index) => index < 10);
 
   firstTen.forEach((element) => {
     let i = document.createElement('i');
-    let price = Math.round(element.priceUsd * 100) / 100
-    i.innerText = `${element.name} (${element.symbol}: ${price})`
+    let price = (Math.round((element.priceUsd * real.brl) * 100) / 100);
+    i.innerText = `${element.name} (${element.symbol}) => R$ ${price}`
     document.getElementById('lista-cripto').appendChild(i)
   })
 }
@@ -25,3 +28,4 @@ const fetchCoins = async () => {
 // Nome da moeda (símbolo da moeda): valor em dólares. Exemplo: Bitcoin (BTC): 46785.06.
 
 window.onload = () => fetchCoins()
+// fetchCoins()
