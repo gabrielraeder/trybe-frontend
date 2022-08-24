388ba4186a16a2d3268bf9b99c885d45bc123ca6
gabrielraeder <gabrielraeder@outlook.com.br>
2022-08-24 15:01:04 -0300

    aula15.5

---
+++ front-end/bloco15/15.5-testes-redux/aula15.5/src/index.js
@@ -0,0 +1,18 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './App';
+
+// Store que foi provida pela nossa aplicação. Nos testes, precisamos prover uma nova store para podermos controlá-la
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
